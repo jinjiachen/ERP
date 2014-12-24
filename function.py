7072f2b03fd1f478940e14018176b57de65b8153
jinjiachen <jinjiachen_tux@163.com>
2014-12-24 16:04:17 +0800

    20141224

--- function.py
+++ function.py
@@ -1,6 +1,6 @@
 #/usr/bin/env python
 #coding=utf-8
-
+from __future__ import division
 
 #输入计算参数
 Pc=input('请输入计算压力(MPa)\n')
@@ -20,5 +20,87 @@ if Pc<=0.4*cigama*fi:
     print float(deltah)
 else:
     print '公式不适用'
+
+
+
+
+####################许用应力计算#########################
+a1='钢管20'
+a2=input('厚度\n')
+a3=input('温度\n')
+if a1=='Q235B':#a1为材料 a2为厚度 a3为温度
+    if a2>=3 and a2<16:
+        if a3<150:
+            ans=113
+        elif a3>=150 and a3<200:
+            ans=(105-113)/50*(a3-150)+113
+        elif a2>=16 and a2<30:
+            if a3<100:
+                ans=113
+            elif a3>=100 and a3<150:
+                ans=(107-113)/50*(a3-100)+113
+            elif a3>=150 and a3<200:
+                ans=(99-107)/50*(a3-150)+107
+elif a1=='Q245R':
+    if a2>=3 and a2<16:
+        if a3<=20:
+            ans=133
+        elif a3>20 and a3<=100:
+            ans=(132-133)/80*(a3-20)+133
+        elif a3>100 and a3<=150:
+            ans=(126-132)/50*(a3-100)+132
+        elif a3>150 and a3<=200:
+            ans=(118-126)/50*(a3-150)+126
+    elif a2>=16 and a2<36:
+        if a3<=20:
+            ans=133
+        elif a3>20 and a3<=100:
+            ans=(126-133)/80*(a3-20)+133
+        elif a3>100 and a3<=150:
+            ans=(120-126)/50*(a3-100)+126
+        elif a3>150 and a3<=200:
+            ans=(112-120)/50*(a3-150)+120
+    elif a2>=36 and a2<60:
+        if a3<=20:
+            ans=133
+        elif a3>20 and a3<=100:
+            ans=(120-133)/80*(a3-20)+133
+        elif a3>100 and a3<=150:
+            ans=(114-120)/50*(a3-100)+120
+        elif a3>150 and a3<=200:
+            ans=(107-114)/50*(a3-150)+114
+elif a1=='钢管20':
+    if a2<=10:
+        if a3<=20:
+            ans=137
+        elif a3>20 and a3<=100:
+            ans=(132-137)/80*(a3-20)+137
+        elif a3>100 and a3<=150:
+            ans=(126-132)/50*(a3-100)+132
+        elif a3>150 and a3<=200:
+            ans=(118-126)/50*(a3-150)+126
+    if a2>10 and a2<=16:
+        if a3<=20:
+            ans=137
+        elif a3>20 and a3<=100:
+            ans=(132-137)/80*(a3-20)+137
+        elif a3>100 and a3<=150:
+            ans=(126-132)/50*(a3-100)+132
+        elif a3>150 and a3<=200:
+            ans=(118-126)/50*(a3-150)+126
+print ans
+
+
+
+############材料下偏差##########
+D=input('外径')
+S=input('厚度')
+if D<=102:
+    dev=max(0.125*S,0.4)
+else:
+    if S/D<=0.05:dev=max(0.15*S,0.4)
+    elif S/D<=0.1:dev=max(0.125*S,0.4)
+    else:dev=max(0.1*S,0.4)
+print dev
 
 
--- volume_calc.py
+++ volume_calc.py
@@ -3,6 +3,7 @@
 
 import wx
 import math
+import fun
 
 pi=3.14159265357
 
@@ -13,27 +14,31 @@ class VolumeFrame(wx.Frame):
         type=['已知参数','未知条件']
         self.singlebox=wx.RadioBox(self.panel,-1,'请选择类型',choices=type)
         #创建初始界面
-        self.text1=wx.StaticText(self.panel,-1,'设计压力(MPa)',(20,80))
+        self.text1=wx.StaticText(self.panel,-1,'容器容积(m^3)',(20,80))
         self.input1=wx.TextCtrl(self.panel,-1,'',(120,80))
-        self.text2=wx.StaticText(self.panel,-1,'焊接系数',(20,120))
+        self.text2=wx.StaticText(self.panel,-1,'设计压力(MPa)',(20,120))
         self.input2=wx.TextCtrl(self.panel,-1,'',(120,120))
-        self.text3=wx.StaticText(self.panel,-1,'选用应力(MPa)',(20,160))
+        self.text3=wx.StaticText(self.panel,-1,'设计温度(℃)',(20,160))
         self.input3=wx.TextCtrl(self.panel,-1,'',(120,160))
-        self.text4=wx.StaticText(self.panel,-1,'容器的容积(m^3)',(20,200))
+        self.text4=wx.StaticText(self.panel,-1,'焊接系数',(20,200))
         self.input4=wx.TextCtrl(self.panel,-1,'',(120,200))
-        self.text5=wx.StaticText(self.panel,-1,'筒体的长度(mm)',(20,240))
-        self.input5=wx.TextCtrl(self.panel,-1,'',(120,240))
-        self.text6=wx.StaticText(self.panel,-1,'筒体的壁厚(mm)',(20,280))
+        self.text5=wx.StaticText(self.panel,-1,'筒体材料',(20,240))
+        mat1=['钢管20','Q245R']
+        self.input5=wx.Choice(self.panel,-1,(120,240),choices=mat1)
+        self.text6=wx.StaticText(self.panel,-1,'筒体内径(mm)',(20,280))
         self.input6=wx.TextCtrl(self.panel,-1,'',(120,280))
-        self.text7=wx.StaticText(self.panel,-1,'筒体的直径(mm)',(20,320))
+        self.text7=wx.StaticText(self.panel,-1,'筒体壁厚(mm)',(20,320))
         self.input7=wx.TextCtrl(self.panel,-1,'',(120,320))
-        self.text8=wx.StaticText(self.panel,-1,'封头尺寸(mm)',(20,360))
+        self.text8=wx.StaticText(self.panel,-1,'封头内径(mm)',(20,360))
         self.input8=wx.TextCtrl(self.panel,-1,'',(120,360))
         self.text9=wx.StaticText(self.panel,-1,'封头壁厚(mm)',(20,400))
         self.input9=wx.TextCtrl(self.panel,-1,'',(120,400))
+        button=wx.Button(self.panel,-1,'确定',(120,440))
 
 
         self.Bind(wx.EVT_RADIOBOX,self.GetIndex,self.singlebox)
+        self.Bind(wx.EVT_BUTTON,self.fun1,button)
+        self.Bind(wx.EVT_CHOICE,self.renew,self.input5)
 
     def GetIndex(self,event):
         if self.singlebox.GetSelection()==0:#不同的选项对应不同的参数
@@ -56,39 +61,28 @@ class VolumeFrame(wx.Frame):
             self.input8.Show(False)
             self.input9.Show(False)
 
-            self.text1=wx.StaticText(self.panel,-1,'设计压力(MPa)',(20,80))
+            self.text1=wx.StaticText(self.panel,-1,'容器容积(m^3)',(20,80))
             self.input1=wx.TextCtrl(self.panel,-1,'',(120,80))
-            self.text2=wx.StaticText(self.panel,-1,'焊接系数',(20,120))
+            self.text2=wx.StaticText(self.panel,-1,'设计压力(MPa)',(20,120))
             self.input2=wx.TextCtrl(self.panel,-1,'',(120,120))
-            self.text3=wx.StaticText(self.panel,-1,'选用应力(MPa)',(20,160))
+            self.text3=wx.StaticText(self.panel,-1,'设计温度(℃)',(20,160))
             self.input3=wx.TextCtrl(self.panel,-1,'',(120,160))
-            self.text4=wx.StaticText(self.panel,-1,'容器的容积(m^3)',(20,200))
+            self.text4=wx.StaticText(self.panel,-1,'焊接系数',(20,200))
             self.input4=wx.TextCtrl(self.panel,-1,'',(120,200))
-            self.text5=wx.StaticText(self.panel,-1,'筒体的长度(mm)',(20,240))
-            self.input5=wx.TextCtrl(self.panel,-1,'',(120,240))
-            self.text6=wx.StaticText(self.panel,-1,'筒体的壁厚(mm)',(20,280))
+            self.text5=wx.StaticText(self.panel,-1,'筒体材料',(20,240))
+            mat1=['钢管20','Q245R']
+            self.input5=wx.Choice(self.panel,-1,(120,240),choices=mat1)
+            self.text6=wx.StaticText(self.panel,-1,'筒体内径(mm)',(20,280))
             self.input6=wx.TextCtrl(self.panel,-1,'',(120,280))
-            self.text7=wx.StaticText(self.panel,-1,'筒体的直径(mm)',(20,320))
+            self.text7=wx.StaticText(self.panel,-1,'筒体壁厚(mm)',(20,320))
             self.input7=wx.TextCtrl(self.panel,-1,'',(120,320))
-            self.text8=wx.StaticText(self.panel,-1,'封头尺寸(mm)',(20,360))
+            self.text8=wx.StaticText(self.panel,-1,'封头内径(mm)',(20,360))
             self.input8=wx.TextCtrl(self.panel,-1,'',(120,360))
             self.text9=wx.StaticText(self.panel,-1,'封头壁厚(mm)',(20,400))
             self.input9=wx.TextCtrl(self.panel,-1,'',(120,400))
+            button=wx.Button(self.panel,-1,'确定',(120,440))
             
 
-
-#            text1=wx.StaticText(self.panel,-1,'容器的容积(m^3)',(20,80))
-#            wx.TextCtrl(self.panel,-1,'',(120,80))
-#            text2=wx.StaticText(self.panel,-1,'筒体的长度(mm)',(20,120))
-#            wx.TextCtrl(self.panel,-1,'',(120,120))
-#            text3=wx.StaticText(self.panel,-1,'筒体的壁厚(mm)',(20,160))
-#            wx.TextCtrl(self.panel,-1,'',(120,160))
-#            text4=wx.StaticText(self.panel,-1,'筒体的直径(mm)',(20,200))
-#            wx.TextCtrl(self.panel,-1,'',(120,200))
-#            text5=wx.StaticText(self.panel,-1,'封头尺寸(mm)',(20,240))
-#            wx.TextCtrl(self.panel,-1,'',(120,240))
-#            text6=wx.StaticText(self.panel,-1,'封头壁厚(mm)',(20,280))
-#            wx.TextCtrl(self.panel,-1,'',(120,280))
         elif self.singlebox.GetSelection()==1:
             self.text1.Show(True)
             self.text2.Show(True)
@@ -115,6 +109,44 @@ class VolumeFrame(wx.Frame):
             self.text7.SetLabel('封头的尺寸(mm)')
             self.text8.SetLabel('封头壁厚(mm)')
             self.text9.SetLabel('容器的容积(m^3)')
+
+    def fun1(self,event):
+        test=self.input5.GetStringSelection().encode('utf-8')
+        print type(test)
+        if test=='Q245R':
+            C=1.3
+        else:
+            D=float(self.input6.GetValue())
+            S=float(self.input7.GetValue())
+            C1=fun.fun3(D,S)
+            C=1+C1
+        Pc=float(self.input2.GetValue())
+        fi=float(self.input4.GetValue())
+        t=float(self.input7.GetValue())
+        tem=float(self.input3.GetValue())
+        cigama=fun.fun2(test,t,tem)
+        deltae=t-C
+        Di=float(self.input6.GetValue())
+        result=fun.fun1(Pc,Di,fi,cigama,deltae,C,t)
+        if result==3:
+            wx.MessageBox('筒体强度不满足!','警告',style=wx.OK)
+        elif result==4:
+            wx.MessageBox('封头强度不满足!','警告',style=wx.OK)
+        elif result==5:
+            wx.MessageBox('筒体封头强度通过!','验证',style=wx.OK)
+        elif result==2:
+            wx.MessageBox('筒体和封头的强度都不满足!',style=wx.OK)
+        elif result==0:
+            wx.MessageBox('公式不适用','警告',style=wx.OK)
+            
+
+    def renew(self,event):
+        if self.input5.GetStringSelection().encode('cp936')=='钢管20':
+            self.text6.SetLabel('筒体外径(mm)')
+            self.text8.SetLabel('封头外径(mm)')
+        else:
+            self.text6.SetLabel('筒体内径(mm)')
+            self.text8.SetLabel('封头内径(mm)')
             
             
 
